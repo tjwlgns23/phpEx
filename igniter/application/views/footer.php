3609e09c82e05b670eafb1793115c99239dda558
tjwlgns23 <81953824+tjwlgns23@users.noreply.github.com>
2021-05-20 18:55:07 +0900

    Delete igniter directory

--- igniter/application/views/footer.php
+++
@@ -1,6 +0,0 @@
-</div>
-    </div>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="/phptest/igniter/static/lib/bootstrap/js/bootstrap.min.js"></script>
-    </body>
-</html>  
--- igniter/application/views/update.php
+++
@@ -1,7 +0,0 @@
-<form action="/phptest/igniter/index.php/topic/update/<?=$topic->id?>" method="post" class="span10">
-    <?php echo validation_errors(); ?>
-    <?php echo "<input type='hidden' name='id' value='{$topic->id}'>"?>
-    <input type="text" name="title" placeholder="제목" class="span12">
-    <textarea name="description" placeholder="본문" class="span12" rows="15"></textarea><br>
-    <input type="submit" class="btn">
-</form>
